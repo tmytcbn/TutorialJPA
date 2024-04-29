5c22d7e235c6343d683c959ae9579ac31cf6d009
tmytcbn <tmytcbn@ymail.ne.jp>
2024-04-30 07:44:54 +0900

    課題提出

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -27,7 +27,7 @@ public class CountryController {
     }
 
     // ---- 詳細画面 ----
-    @GetMapping(value = { "/detail", "/detail/{code}/" })
+    @GetMapping(value = { "/detail", "/detail/{code}" })
     public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
         // codeが指定されていたら検索結果、無ければ空のクラスを設定
         Country country = code != null ? service.getCountry(code) : new Country();
@@ -49,8 +49,12 @@ public class CountryController {
     }
 
     // ---- 削除画面 ----
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = { "/delete", "/delete/{code}" })
+    public String deleteCountryForm(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country);
         // country/delete.htmlに画面遷移
         return "country/delete";
     }
